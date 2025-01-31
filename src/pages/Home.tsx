877492c528e3f7fc70cc7baf0882a06e5f0a75fa
Qi <chenqilun0519@gmail.com>
2025-01-31 16:12:30 -0500

    fixed layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 
 const Home: React.FC = () => {
   return (
-    <body>
     <div className="top-container">
         <h1>I am Qilun Chen</h1>
       <main>
         <p>a programmer.</p>
       </main>
     </div>
-    </body>
   );
 };
 
